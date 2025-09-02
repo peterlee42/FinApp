b3ac836d4779bad48bc7767bff044bb3967db288
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-09-02 16:48:21 -0400

    set up prisma. ready for development

--- src/app.js
+++ src/app.js
@@ -1,22 +1,119 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 const app = express();
-const prisma = new PrismaClient();
 
-async function main() {
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
+app.use(express.json());
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
+app.post(`/signup`, async (req, res) => {
+  const { name, email, posts } = req.body;
+
+  const postData = posts?.map((post) => {
+    return { title: post?.title, content: post?.content };
   });
+
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      posts: {
+        create: postData,
+      },
+    },
+  });
+  res.json(result);
+});
+
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
+
+app.put('/post/:id/views', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: {
+        viewCount: {
+          increment: 1,
+        },
+      },
+    });
+
+    res.json(post);
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
+});
+
+app.put('/publish/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const postData = await prisma.post.findUnique({
+      where: { id: Number(id) },
+      select: {
+        published: true,
+      },
+    });
+
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(id) || undefined },
+      data: { published: !postData?.published },
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
+});
+
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(post);
+});
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.get('/user/:id/drafts', async (req, res) => {
+  const { id } = req.params;
+
+  const drafts = await prisma.post.findMany({
+    where: {
+      authorId: Number(id),
+      published: false,
+    },
+  });
+
+  res.json(drafts);
+});
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params;
+
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
 
 export { app };
